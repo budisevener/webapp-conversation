378491283b8262d107e14384924f423490b024bc
budisevener <budisevener@gmail.com>
2025-08-30 19:34:12 +0700

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,8 +3,8 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
+  title: 'POC Web APP RAG v0.1',
+  description: 'POC untuk web APP RAG',
   copyright: '',
   privacy_policy: '',
   default_language: 'en',
